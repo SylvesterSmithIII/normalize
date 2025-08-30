207fba43691f0689cdf203cf2c51633d05505044
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2025-08-30 15:11:48 -0700

    bottom row

--- components/FirstRow.jsx
+++ components/FirstRow.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Row2 from "../images/Row2.PNG";
 import ShirtsRow1 from "./ShirtsRow1";
 import ImageRow from "./ImageRow";
+import BottomImage from "../images/BottomImage.jpeg";
 
 
 
@@ -57,6 +58,37 @@ export default async function FirstRow() {
       },
     ]
 
+    const secondRowTShirts = [
+      {
+        cover1: "https://images-api.printify.com/mockup/6894f93110430a23e7100ef4/12100/92570/red-bra-tee.jpg?camera_label=front",
+        cover2: "https://images-api.printify.com/mockup/6894f93110430a23e7100ef4/12100/92571/red-bra-tee.jpg?camera_label=back",
+        name: "Red Bra Tee",
+        price: "20.00",
+        id: "6894f93110430a23e7100ef4"
+      },
+      {
+        cover1: "https://images-api.printify.com/mockup/68951c08494f5a8a9a0dda74/12100/92570/new-york-flower-tee.jpg?camera_label=front",
+        cover2: "https://images-api.printify.com/mockup/68951c08494f5a8a9a0dda74/12100/92571/new-york-flower-tee.jpg?camera_label=back",
+        name: "New York Flower Tee",
+        price: "20.00",
+        id: "68951c08494f5a8a9a0dda74"
+      },
+      {
+        cover1: "https://images-api.printify.com/mockup/68952214a545d9b6950a4022/12124/92570/i-love-who-i-am-tee.jpg?camera_label=front",
+        cover2: "https://images-api.printify.com/mockup/68952214a545d9b6950a4022/12124/92571/i-love-who-i-am-tee.jpg?camera_label=back",
+        name: "I Love Who I Am Tee",
+        price: "20.00",
+        id: "68952214a545d9b6950a4022"
+      },
+      {
+        cover1: "https://images-api.printify.com/mockup/6894f9fb8a6d7e74790c1fb8/12100/92570/choose-to-be-kind-butterfly-tee.jpg?camera_label=front",
+        cover2: "https://images-api.printify.com/mockup/6894f9fb8a6d7e74790c1fb8/12100/92571/choose-to-be-kind-butterfly-tee.jpg?camera_label=back",
+        name: "Choose To Be Kind Butterfly Tee",
+        price: "20.00",
+        id: "6894f9fb8a6d7e74790c1fb8"
+      },
+    ]
+
     return (
         // <div className="flex flex-col items-center justify-center h-[75vh] ">
         //     {/* <h1 >SHOP FEATURED PRODUCTS</h1> */}
@@ -72,7 +104,7 @@ export default async function FirstRow() {
         <div className=" bg-white">
 
 
-        <ShirtsRow1 tShirts={firstRowTShirts} collection={"The Fur Collection"} />
+        <ShirtsRow1 tShirts={firstRowTShirts} collection={"The Fur Collection"} className="bg-white" style={{}} />
 
 
 
@@ -82,14 +114,38 @@ export default async function FirstRow() {
        
         <ImageRow />
 
-<div className="flex flex-col items-center justify-center h-[75vh] ">
-{/* <h1 >SHOP FEATURED PRODUCTS</h1> */}
-<div className="grid grid-cols-4 gap-1 w-full h-[70vh] p-4">
-{tShirts.map((product) => (
-   <ItemCard key={product.id} product={product} />
- ))}
- </div>
-</div>
+        <ShirtsRow1 tShirts={secondRowTShirts} collection={"The Self Love Club"} className="bg-[#FFF7E9]" style={{
+          backgroundImage: `
+            repeating-linear-gradient(
+              to right,
+              #000 0px,
+              #000 1px,
+              transparent 1px,
+              transparent 7px,
+              #000 7px,
+              #000 8px,
+              transparent 8px,
+              transparent 100px
+            )
+          `
+        }} />
+
+         <div className="w-full h-[80vh] flex">
+
+        
+
+              <div className="relative flex-1">
+                <Image
+                  src={BottomImage}
+                  alt="Bottom Image"
+                  fill
+                  className="object-cover"
+                  priority
+                />
+              </div>
+            </div>
+
+
     </>
     );
 };
--- components/ShirtsRow1.jsx
+++ components/ShirtsRow1.jsx
@@ -7,19 +7,19 @@ const alexBrush = Alex_Brush({
 })
 
 
-export default function ShirtsRow1 ({ tShirts, collection }) {
+export default function ShirtsRow1 ({ tShirts, collection, style, className }) {
 
 
 
     return (
-        <div className="flex flex-col h-[70vh] bg-white items-center justify-between pt-8 pb-12">
+        <div className={`flex flex-col h-[70vh] items-center justify-between pt-8 pb-12 ${className}`} style={style}>
 
             <h1 className={`${alexBrush.className} text-amber-950 text-6xl`}> {collection} </h1>
 
             <div className="flex justify-evenly w-full gap-2 place-items-stretch px-4">
 
                 {tShirts.map((product) => (
-                    <div className="aspect-[2/3] w-[20vw]">
+                    <div key={product.id} className="aspect-[2/3] w-[20vw]">
                         <ItemCard key={product.id} product={product} />
                     </div>
                 ))}
