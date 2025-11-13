ed51ed0aafaa0ac332efff98f93a045da1d18f4f
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2025-11-12 18:22:11 -0800

    test

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -82,7 +82,6 @@ export async function POST(req) {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items,
-      automatic_tax: { enabled: true },
       mode: "payment",
       success_url: `${req.headers.get("origin")}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${req.headers.get("origin")}`,
